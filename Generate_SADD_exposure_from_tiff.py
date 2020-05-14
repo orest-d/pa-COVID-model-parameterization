63c9bc520e38ac0918dc254769a7e6401dfe1db5
Leonardo Milano <milanoleonardo@yahoo.it>
2020-05-14 14:18:58 +0200

    fixing typo in tif file name

--- Generate_SADD_exposure_from_tiff.py
+++ Generate_SADD_exposure_from_tiff.py
@@ -13,8 +13,8 @@ country_iso3='AFG'
 if country_iso3=='AFG':
     INPUT_SHP = 'Inputs/Shapefiles/afg_admbnda_adm2_agcho_20180522/afg_admbnda_adm2_agcho_20180522.shp'
     INPUT_TIFF_SADD = 'Inputs/WorldPop/afg_{}_{}_2020.tif'
-    INPUT_TIFF_POP = 'Inputs/WorldPop/afg_pop_2020.tif'
-    INPUT_TIFF_POP_UNadj='Inputs/WorldPop/afg_pop_2020_UNadj.tif'
+    INPUT_TIFF_POP = 'Inputs/WorldPop/afg_ppp_2020.tif'
+    INPUT_TIFF_POP_UNadj='Inputs/WorldPop/afg_ppp_2020_UNadj.tif'
 
 OUTPUT_SHP = 'Outputs/Exposure_SADD/{}_Exposure.shp'.format(country_iso3)
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -28,17 +28,17 @@ gender_age_groups=list(itertools.product(gender_classes,age_classes))
 for gender_age_group in gender_age_groups:
     gender_age_group_name='{}_{}'.format(gender_age_group[0],gender_age_group[1])
     print('analyising gender age ',gender_age_group_name)
-    zs = zonal_stats(INPUT_SHP,INPUT_TIFF_SADD.format(gender_age_group[0],gender_age_group[1]),stats='sum')
+    zs = zonal_stats(dir_path+INPUT_SHP,dir_path+INPUT_TIFF_SADD.format(gender_age_group[0],gender_age_group[1]),stats='sum')
     total_pop=[district_zs.get('sum') for district_zs in zs]
     ADM2boundaries[gender_age_group_name]=total_pop
 
 # total population for cross check
-zs = zonal_stats(INPUT_SHP,INPUT_TIFF_POP,stats='sum')
+zs = zonal_stats(dir_path+INPUT_SHP,dir_path+INPUT_TIFF_POP,stats='sum')
 total_pop=[district_zs.get('sum') for district_zs in zs]
 ADM2boundaries['tot_pop']=total_pop
 
 # total population UNadj for cross check
-zs = zonal_stats(INPUT_SHP,INPUT_TIFF_POP_UNadj,stats='sum')
+zs = zonal_stats(dir_path+INPUT_SHP,dir_path+INPUT_TIFF_POP_UNadj,stats='sum')
 total_pop=[district_zs.get('sum') for district_zs in zs]
 ADM2boundaries['tot_pop_UNadj']=total_pop
 
