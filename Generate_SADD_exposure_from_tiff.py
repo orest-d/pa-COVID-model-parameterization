b925e98bf72a44a1d03a48548ba1c79be27d7c9e
Monica Turner <turnermon@gmail.com>
2020-05-25 15:49:36 +0200

    Removed debugging variables

--- Generate_SADD_exposure_from_tiff.py
+++ Generate_SADD_exposure_from_tiff.py
@@ -25,8 +25,6 @@ CO_DIR = 'InputsFromCOs'
 
 GENDER_CLASSES = ["f","m"]
 AGE_CLASSES = [0,1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80]
-GENDER_CLASSES = ["f"]
-AGE_CLASSES = [0]
 WORLDPOP_DIR = 'WorldPop'
 WORLDPOP_FILENAMES = {
     'sadd':  '{country_iso3}_{gender}_{age}_2020.tif',
