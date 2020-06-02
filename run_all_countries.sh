ea9ab5115831fe49ab16e0cad58422b0e42a5fb8
Leonardo Milano <milanoleonardo@yahoo.it>
2020-06-02 16:02:41 +0200

    adding file to run all countries

---
+++ run_all_countries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for iso3 in AFG SSD SDN COD HTI
+do
+    python Generate_SADD_exposure_from_tiff.py $iso3
+    python Generate_vulnerability_file.py $iso3
+    python Generate_COVID_file.py $iso3
+    python Generate_graph.py $iso3
+done
