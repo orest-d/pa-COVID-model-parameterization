87fe7ecaa530f4d1359a68559a67943a7bb26efb
Monica Turner <turnermon@gmail.com>
2020-07-01 11:01:48 +0200

    Generated final NPI files

--- Generate_NPIs.py
+++ Generate_NPIs.py
@@ -2,9 +2,12 @@ import os
 import logging
 from pathlib import Path
 import argparse
+from datetime import datetime
 
 import pandas as pd
 import geopandas as gpd
+import xarray as xr
+import numpy as np
 
 from utils import utils
 from utils.hdx_api import query_api
@@ -13,6 +16,7 @@ CONFIG_FILE = 'config.yml'
 
 ACAPS_HDX_ADDRESS = 'acaps-covid19-government-measures-dataset'
 INPUT_DIR = 'Inputs'
+OUTPUT_DIR = 'Outputs'
 
 RAW_DATA_DIR = os.path.join(INPUT_DIR, 'ACAPS_NPIs')
 RAW_DATA_FILENAME = 'ACAPS_npis_raw_data.xlsx'
@@ -20,6 +24,7 @@ RAW_DATA_FILEPATH = os.path.join(RAW_DATA_DIR, RAW_DATA_FILENAME)
 
 OUTPUT_DATA_DIR = 'NPIs'
 OUTPUT_JSON_FILENAME = '{}_NPIs.json'
+OUTPUT_CSV_FILENAME = '{}_NPIs.csv'
 
 MEASURE_EQUIVALENCE_FILENAME = 'NPIs - ACAPS NPIs.csv'
 
@@ -37,7 +42,7 @@ def parse_args():
 
 def main(download):
     config = utils.parse_yaml(CONFIG_FILE)
-    countries = list(config.keys())[0:1]
+    countries = list(config.keys())
     # Get ACAPS and Natural Earth data
     if download:
         logger.info('Getting ACAPS data')
@@ -47,7 +52,7 @@ def main(download):
     # Take only the countries of concern
     df_acaps = df_acaps[df_acaps['ISO'].isin(countries)]
     # Simplify LOG_TYPE
-    df_acaps = df_acaps.replace({'Introduction / extension of measures': 'implement',
+    df_acaps = df_acaps.replace({'Introduction / extension of measures': 'add',
                                 'Phase-out measure': 'remove'})
     # Get our measures equivalent, and drop any that we don't use
     measures_dict = get_measures_equivalence_dictionary()
@@ -116,19 +121,46 @@ def get_country_info(country_iso3, df_acaps, boundaries):
     for row in df.itertuples():
         loc_list = df.at[row.Index, 'affected_pcodes']
         final_loc_list = []
-        for loc in loc_list:
-            if loc in admin_regions['admin1']:
-                final_loc_list += admin1_to_2_dict[loc]
-            elif loc in admin_regions['admin2']:
-                final_loc_list.append(loc)
-            else:
-                logger.error(f'Found incorrect pcode {loc}')
+        try:
+            for loc in loc_list:
+                if loc in admin_regions['admin1']:
+                    final_loc_list += admin1_to_2_dict[loc]
+                elif loc in admin_regions['admin2']:
+                    final_loc_list.append(loc)
+                else:
+                    logger.error(f'Found incorrect pcode {loc}')
+        # If loc_list is not a list
+        except TypeError:
+            continue
         df.at[row.Index, 'affected_pcodes'] = final_loc_list
     return df
 
 
 def write_country_info_to_csv(country_iso3, df, boundaries):
-    pass
+    # Create 3d xarray
+    coords = {
+        'date': pd.date_range(df['ENTRY_DATE'].min(), datetime.today()),
+        'measure': sorted(df['our_measures'].unique()),
+        'admin2': sorted(boundaries['ADM2_PCODE'].unique()),
+    }
+    da = xr.DataArray(np.zeros([len(val) for val in coords.values()], dtype=int),
+                      dims=coords.keys(), coords=coords)
+    # Only take rows with locations
+    df = df[df['affected_pcodes'].notna()]
+    if df.empty:
+       logger.warning(f'No location information available for {country_iso3}, output file will just have 0s')
+    # Populate it by looping through the dataframe
+    for _, row in df.iterrows():
+        date_range = pd.date_range(row['ENTRY_DATE'], datetime.today())
+        value_to_add = 1 if row['LOG_TYPE'] == 'add' else -1
+        da.loc[date_range, row['our_measures'], row['affected_pcodes']] += value_to_add
+    # Convert to dataframe and write out
+    df_out = da.to_dataframe('result').unstack().droplevel(0, axis=1)
+    output_dir = os.path.join(OUTPUT_DIR, country_iso3, 'NPIs')
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    filename = os.path.join(output_dir, OUTPUT_CSV_FILENAME.format(country_iso3))
+    logger.info(f'Writing final results to {filename}')
+    df_out.to_csv(filename)
 
 
 if __name__ == '__main__':
